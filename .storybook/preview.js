0dccff9d6f11a26c082af82e786cf010a8c7e638
Idris <idris.taiwo@me.com>
2023-04-23 15:16:27 +0100

    setup storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { withPerformance } from "storybook-addon-performance";
+
+addDecorator(withPerformance);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface MyComponentProps {
+  // Add your component props here
+}
+
+export const MyComponent: React.FC<MyComponentProps> = (props) => {
+  return <h1>button</h1>;
+};
